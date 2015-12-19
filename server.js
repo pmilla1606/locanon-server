2bf7c733b8d44aa9d3b025dc77506170dc43a51b
pmilla1606 <pmargaritoff@gmail.com>
2015-12-19 15:29:02 -0500

    openshift mongo?

--- server.js
+++ server.js
@@ -19,6 +19,20 @@ var oneDay = 86400000;
 
 server.listen(1337);
 
+var ip_addr = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
+var port    = process.env.OPENSHIFT_NODEJS_PORT || '8080';
+
+// default to a 'localhost' configuration:
+var connection_string = 'mongodb://localhost/locanon_dev_db';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
 mongoose.connect('mongodb://localhost/locanon_dev_db');
 var db = mongoose.connection;
 
